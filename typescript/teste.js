58580133fc815422cfb5675193fd7956cb40b04e
henrygoncalvess <octanebt@gmail.com>
2024-10-02 20:54:23 -0300

    o último arquivo JS

--- typescript/teste.js
+++
@@ -1,9 +0,0 @@
-var Curso = /** @class */ (function () {
-    function Curso(canal, curso) {
-        this.canal = null;
-        this.curso = null;
-        this.canal = canal;
-        this.curso = canal;
-    }
-    return Curso;
-}());
